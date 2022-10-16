67e30fb22d5056cd00f0252f9304576bbc69b250
Zuleykha Pavlichenkova <hmeriann@student.21-school.ru>
2022-10-16 22:58:55 +0300

    functions HW is done

--- 1_6_Functions/1_6_Functions.playground/Contents.swift
+++ 1_6_Functions/1_6_Functions.playground/Contents.swift
@@ -1,13 +1,4 @@
 import Foundation
-//3.
-//4. функция принимает число на вход (год), проверить високосный ли он
-
-// Есть произвольный массив чисел
-var array: [Int] = []
-for _ in 0..<20 {
-    array.append(Int.random(in: -20...100))
-}
-//print(array)
 
 // MARK: ex01 - Sum of two Ints
 // функция складывает две целочисленных переменных - отдает на выходе сумму
@@ -15,7 +6,8 @@ func twoIntsSum(first: Int, second: Int) -> Int {
     return first + second
 }
 
-//print(twoIntsSum(first: 0, second: -98765643))
+print("1) функция складывает две целочисленных переменных - отдает на выходе сумму:")
+print(twoIntsSum(first: 0, second: -9))
 
 
 // MARK: ex02 - String from tuple
@@ -25,32 +17,36 @@ func makeStringFromTuple(from pair:(Int, String)) -> String {
     return String(pair.0) + " " + pair.1
 }
 
-//print(makeStringFromTuple(from: (5, "мороженок")))
+print("\n2) функция принимает кортеж из числа и строки приводит число к строке и ввыводит в консоль результат:")
+print(makeStringFromTuple(from: (5, "мороженок")))
+
 
 // MARK: ex03 - Optional closure
 // функция принимает на вход опциональное замыкание и целое число, выполняет замыкание только в случае если число больше 0
 
-var dict = ["ann": "password",
-            "lizzy": "Access7forbidden",
-            "john404": "File not found",
-            "cole500": "Internal server error"]
-
-func findUsernameWithLongerPassword(from dict: [String: String]) -> [String] {
-    var result: [String] = []
-    
-    for v in dict.values {
-        if v.count >= 10 {
-            result.append(v)
-        }
+func exampleWithClosure(with closure: ( () -> Int )?, and number: Int) {
+    if number > 0 {
+        closure?()
     }
-    
-    return result
 }
 
-//var arr = findUsernameWithLongerPassword(from: dict)
-//
-//dict.forEach { print($0) }
-//print()
-//
-//arr.forEach { print($0) }
-//print()
+print("\n3) функция принимает на вход опциональное замыкание и целое число, выполняет замыкание только в случае если число больше 0")
+var num = 10
+// не выполнится
+exampleWithClosure(with: nil, and: num)
+//выполнится
+exampleWithClosure(with: {print("The number passed id >= 10 and equals to: \(num)"); return 0}, and: num)
+
+
+// MARK: ex04 - Optional closure
+// функция принимает число на вход (год), проверить високосный ли он
+
+func isYearLeap(with year: Int) -> Bool {
+    return year % 4 == 0
+}
+
+print("\n4) функция принимает число на вход (год), проверить високосный ли он")
+let leapYear = 1984
+let year = 1989
+print("Is the \(leapYear) a leap year? - \(isYearLeap(with: leapYear))")
+print("Is the \(year) a leap year? - \(isYearLeap(with: year))")
