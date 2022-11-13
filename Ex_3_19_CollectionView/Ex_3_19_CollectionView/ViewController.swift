3fc478e03dfffafed60333e1d18cd50cb5dc4e67
Zuleykha Pavlichenkova <hmeriann@student.21-school.ru>
2022-11-14 01:17:23 +0300

    something went wring

--- Ex_3_19_CollectionView/Ex_3_19_CollectionView/ViewController.swift
+++ Ex_3_19_CollectionView/Ex_3_19_CollectionView/ViewController.swift
@@ -4,34 +4,21 @@
 //
 //  Created by Zuleykha Pavlichenkova on 13.11.2022.
 //
-// 1. Добавьте на UIViewController UICollectionView
+// 1. Добавьте на UIViewController UCollectionView
 // 2. Создайте класс-помощник, в котором реализуйте протоколы для работы с UCollectionView. Количество столбцов на ваш выбор, но больше 1.
-// 3. Добавьте модель данных, которая будет использоваться с сеткой. Класс или структура - на ваш выбор
-// 4. Добавьте UICollectionViewCell. Элемент должен содержать UIImageView. Можно добавить еще пару UILabel или других контролов по желанию. Не забудьте метод для настройки cell.
+// 3. Добавьте модель данных, которая будет использоваться со сеткой. Класс или структура - на ваш выбор
+// 4. Добавьте UICollectionViewCell. Элемент должен содержан UIImageView. Можно добавить еще пару UILabel или других контролов по желанию. Не забудьте метод для настройки cell.
 // 5. Соедините все элементы, чтобы изображения выводились в сетку.
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDataSource {
+class ViewController: UIViewController {
 
-    // here was  weak
-    @IBOutlet var collectionView: UICollectionView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 25
-    }
-    
 
-    
 }
 
