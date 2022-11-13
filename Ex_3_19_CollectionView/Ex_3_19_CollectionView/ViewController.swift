70b7f4d4acabaed9cb2276635a71c0446fe36d6e
Zuleykha Pavlichenkova <hmeriann@student.21-school.ru>
2022-11-13 23:59:39 +0300

    Added Label into a cell, did hardcoded size of the cell

--- Ex_3_19_CollectionView/Ex_3_19_CollectionView/ViewController.swift
+++ Ex_3_19_CollectionView/Ex_3_19_CollectionView/ViewController.swift
@@ -23,12 +23,12 @@ class ViewController: UIViewController, UICollectionViewDataSource {
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = UICollectionViewCell()
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return 25
     }
     
 
