6f4db26f71ddb6cf98217360f0d379c52ed585e2
Zuleykha Pavlichenkova <zuleykhapavlichenkova@Zuleykhas-MacBook-Pro.local>
2022-12-18 12:26:31 +0300

    Empty app

---
+++ Ex_4_24_URLSession/Ex_4_24_URLSession/App/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Ex_4_24_URLSession
+//
+//  Created by Zuleykha Pavlichenkova on 18.12.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
